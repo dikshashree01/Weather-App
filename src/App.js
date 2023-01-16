185ca2882c9b4482b8569cf25c624156b7c61c50
Diksha Shree <89994232+dikshashree01@users.noreply.github.com>
2023-01-16 23:52:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+ import Temp from "./components/weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
+
+
